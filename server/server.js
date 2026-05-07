9a078d66d7cffbb95e7ec47dc09489f2eff24807
Ainsley Meldrum <ainsley2007@hotmail.com>
2026-05-07 13:02:45 +0100

    input server.js

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Create the connection to phpMyAdmin (MySQL)
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tech-support'
+});
+
+app.get('/api/user/:username', (req, res) => {
+    const sql = "SELECT * FROM users WHERE username = ?";
+    db.query(sql, [req.params.username], (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+useEffect(() => {
+    fetch('http://localhost:5000/api/user/${username}')
+        .then(res => res.json())
+        .then(data => {
+            if (data.length > 0) {
+                // Assuming your 'progress' field is a JSON string in MySQL
+                const userProgress = JSON.parse(data[0].progress);
+                setProgress(userProgress);
+            }
+        })
+        .catch(err => console.error('Database connection error:', err));
+}, [username]);
